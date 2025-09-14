e25742842ace273da59129dd7e361833f2601a5b
dimitris manousos <mandimastro@gmail.com>
2025-09-14 19:04:36 +0300

    Start implementing open position simulation function in env.

--- trading_env.py
+++ trading_env.py
@@ -5,16 +5,30 @@ class TradingEnv:
     def __init__(self, candles_file: str, tick_file: str):
         self.candles_file = candles_file
         self.tick_file = tick_file
-        self.tick_line = 0
+        self.current_tick = 0
         self.position_open = False
 
-    def open_position(self, position_name, sl_pips, tp_pips):
+    def get_tick(self):
+        tick = None        
+        for chunk in pd.read_csv(self.tick_file, chunksize=1000):
+            if self.current_tick in chunk.index:
+                tick = chunk.loc[self.current_tick]
+                break
+        return tick
+
+    def open_position(self, position_name: str, sl_pips: float, tp_pips: float):
         
         ask_value = 0
+        bid_value = 0
+        self.position_open = True
 
         # Get the name and identify if it is a BUY or SELL position
         if position_name == "BUY":
-            ask_value = self.tick_file
+            tick_row = self.get_tick()
+            while self.get_tick():
+                ask_value = tick_row["Ask price"]
+                print(f"Ask price: {ask_value}")
+                self.current_tick+=1
         
         # Get the current tick's ask or bid price depending of position and save it to a variable
         
@@ -31,5 +45,4 @@ class TradingEnv:
 if __name__ == '__main__':
 
     env = TradingEnv('EURUSD_Daily.csv', 'EURUSD_ticks.csv')
-    df = pd.read_csv(env.tick_file, nrows=100)
-    print(df)
+    env.open_position("BUY", 50, 50)
