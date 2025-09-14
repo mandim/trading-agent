bb0fb8dbf11d9284bfddfce083453ace92b75664
dimitris manousos <mandimastro@gmail.com>
2025-09-14 18:11:26 +0300

    Initial commit

---
+++ trading_env.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+class TradingEnv:
+
+    def __init__(self, candles_file, tick_file):
+        self.candles_file = candles_file
+        self.tick_file = tick_file
+        self.tick_line = 0
+        self.position_open = False
+
+    def open_position(self, position_name, sl_pips, tp_pips):
+        
+        ask_value = 0
+
+        # Get the name and identify if it is a BUY or SELL position
+        if position_name is "BUY":
+            ask_value = self.tick_file
+        
+        # Get the current tick's ask or bid price depending of position and save it to a variable
+        
+        # Calculate the SL and TP by add or sub from the current price the SL/TP pips
+        
+        # Start comparing every tick_line each tick current price, increment the tick_line
+
+        # If tick_line goes to the end of file then close the position
+        
+        # If the tick current price equals to TP/SL then close the posistion
+
+        # Calculate profit
+
+if __name__ == '__main__':
+
+    env = TradingEnv('EURUSD_Daily.csv')
+    df = pd.read_csv(env.candles_file)
+    print(df)
